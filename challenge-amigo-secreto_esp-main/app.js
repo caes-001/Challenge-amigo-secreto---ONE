7e23b01f255aa01af4e9300ba647530c8cc7b27d
Carlos  A.  Santiago <zetta.rhuk.002@gmail.com>
2025-03-04 19:16:00 -0600

    creacion del array y funciones add en el js

--- challenge-amigo-secreto_esp-main/app.js
+++ challenge-amigo-secreto_esp-main/app.js
@@ -1,1 +1,15 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+//creacion del array que almacenara los nombres de los amigos
+let = amigos[];
+
+//fucion que agregara los nombres al array 
+function agregarAmigos(){
+    const input=document.getElementById("amigo"); //se obtiene el elemento de DOM mediante el id "amigo"
+    const nombre = input.value.trim(); //se obtiene la informacion del campo de entrada y se quitan los espacios en blanco al inicio y al final
+
+//verificamos la condición para entradas vacías o con solo espacios.
+    if (nombre){ 
+        amigos.push(nombre);
+        actualizarLista();
+        input.value="";
+    }
+}
