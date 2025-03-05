e7e00fd9c1bad45d0ccaaeb75ed07ca699ca421e
Carlos  A.  Santiago <zetta.rhuk.002@gmail.com>
2025-03-05 13:56:55 -0600

    creacion de la funcion para generar la lista de amigos  y funcion de sorteo

--- challenge-amigo-secreto_esp-main/app.js
+++ challenge-amigo-secreto_esp-main/app.js
@@ -13,3 +13,28 @@ function agregarAmigos(){
         input.value="";
     }
 }
+
+//accedemos al DOM y obtenemos el nodo de id 'listaAmigos'
+function actualizarLista() {
+    const lista = document.getElementById('listaAmigos');
+    lista.innerHTML = '';  //aqui vaciamos el contenido actual dentro de `lista`
+//creacion de la funcion que nos permite iterar el array de amigos y añadir lis nuevos elementos en 'li'
+    amigos.forEach((amigo, index) => {  //
+        const li = document.createElement('li');
+        li.textContent = amigo;
+        lista.appendChild(li);
+    });
+}
+//creacion de la funcion que sorteara amigos 
+function sortearAmigo() {
+    if (amigos.length === 0) {  //verificamos si esta vacio el array 
+        alert("ingresa al menos a 2 amigos para sortear");  //definimos el mensaje del alert en caso de estar vacio
+        return;
+    }
+
+    const indiceAleatorio = Math.floor(Math.random() * amigos.length);  //generamos el numero aleatorio
+    const amigoSorteado = amigos[indiceAleatorio];  //seleccionamos el amigos aleatorio 
+
+    const resultado = document.getElementById('resultado');
+    resultado.innerHTML = `<li>${amigoSorteado}</li>`;  //agregamos el resultado a una lista no ordenada
+}
